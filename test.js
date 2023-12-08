5e0b43689822cebb09f2a421088059858a540d04
b8591340 <70052269+b8591340@users.noreply.github.com>
2023-12-08 15:11:29 +0100

    convert to esm

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 /* eslint-disable no-console */
 "use strict";
 
-const fs = require("fs").promises;
-const path = require("path");
+import fs from "fs/promises";
+import path from "path";
 
-const assert = require("assert");
+import assert from "assert";
 
-const phash = require("./index");
-const dist = require("./distance");
+import phash from "./index.js";
+import dist from "./distance.js";
 
 const lenna_png = "Lenna.png";
 const lenna_jpg = "Lenna.jpg";
@@ -25,26 +25,24 @@ const fb5 = "fb5.jpg";
 const fb6 = "fb6.jpg";
 
 function getPHash(img) {
-  return fs.readFile(path.join(".", "img", img)).then((buf) => phash(buf));
+	return fs.readFile(path.join(".", "img", img)).then((buf) => phash(buf));
 }
 
 function bitCount(hash) {
-  return hash.replace(/0/g, "").length;
+	return hash.replace(/0/g, "").length;
 }
 
 function testCase(img1, img2, cond) {
-  return Promise.all([getPHash(img1), getPHash(img2)]).then(
-    ([hash1, hash2]) => {
-      const d = dist(hash1, hash2);
-      const text = `${img1} vs ${img2}
+	return Promise.all([getPHash(img1), getPHash(img2)]).then(([hash1, hash2]) => {
+		const d = dist(hash1, hash2);
+		const text = `${img1} vs ${img2}
 hash1: ${hash1} (${bitCount(hash1)})
 hash2: ${hash2} (${bitCount(hash2)})
 distance: ${d}
 `;
-      assert.ok(cond(d), text);
-      console.log("Test PASS", img1, img2, cond.name);
-    }
-  );
+		assert.ok(cond(d), text);
+		console.log("Test PASS", img1, img2, cond.name);
+	});
 }
 
 const SIMILAR = (d) => d <= 5;
@@ -52,15 +50,15 @@ const LIKELY_SIMILAR = (d) => d <= 10;
 const NOT_SIMILAR = (d) => d > 10;
 
 Promise.all([
-  testCase(lenna_png, lenna_jpg, SIMILAR),
-  testCase(lenna_jpg, lenna_sepia, SIMILAR),
-  testCase(lenna_jpg, lenna_exif, SIMILAR),
-  testCase(fb, xing, LIKELY_SIMILAR),
-  testCase(fb1, fb2, NOT_SIMILAR),
-  testCase(fb1, lenna_jpg, NOT_SIMILAR),
-  testCase(fb3, fb4, NOT_SIMILAR),
-  testCase(fb5, fb6, NOT_SIMILAR),
+	testCase(lenna_png, lenna_jpg, SIMILAR),
+	testCase(lenna_jpg, lenna_sepia, SIMILAR),
+	testCase(lenna_jpg, lenna_exif, SIMILAR),
+	testCase(fb, xing, LIKELY_SIMILAR),
+	testCase(fb1, fb2, NOT_SIMILAR),
+	testCase(fb1, lenna_jpg, NOT_SIMILAR),
+	testCase(fb3, fb4, NOT_SIMILAR),
+	testCase(fb5, fb6, NOT_SIMILAR),
 ]).catch((err) => {
-  console.log("Test fail");
-  console.log(err.message);
+	console.log("Test fail");
+	console.log(err.message);
 });
